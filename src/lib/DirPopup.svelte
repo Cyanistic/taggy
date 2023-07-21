ab447fed103d4de0c60467b8b5db25e03be274e4
Cyanism <61608977+Cyanistic@users.noreply.github.com>
2023-07-20 23:09:34 -0400

    ui fixes

--- src/lib/DirPopup.svelte
+++ src/lib/DirPopup.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import { open } from "@tauri-apps/api/dialog"
   import { createEventDispatcher } from "svelte";
+  import { fade } from "svelte/transition"
   const dispatch = createEventDispatcher();
 
   async function getDir(){
@@ -13,6 +14,13 @@
   }
 </script>
 
-<div class="flex justify-end border-gray-500 border-4">
-    <button class="bg-gray-100 dark:bg-gray-900 text-gray-900 dark:text-gray-100" on:click={getDir}>Select Your Music Directory</button>
+<div class="flex h-screen w-screen justify-center items-center">
+  <div in:fade={{}} class="justify-center items-center w-1/2 h-1/2 border-white border-2 rounded-xl flex flex-col bg-gray-900 gap-4">
+    <img src="/default.png" alt="">
+    <h1 class="text-3xl font-bold">Welcome to Taggy!</h1>
+    <h2 class="text-xl">Select your music directory to get started.</h2>
+    <button class="bg-gray-100 dark:bg-gray-900 text-gray-900 dark:text-gray-100" on:click={getDir}>Select</button>
+  </div>
 </div>
+
+
--- src/lib/MainWindow.svelte
+++ src/lib/MainWindow.svelte
@@ -31,7 +31,12 @@
   }
 
   onMount(async () => {
-    options = JSON.parse(localStorage.getItem("options"))
+    if (JSON.parse(localStorage.getItem("options")) != null){
+      options = JSON.parse(localStorage.getItem("options"))
+    }else{
+      options = {}
+      options.recursive = false
+    }
     loadSongs()
     const theme = localStorage.getItem("theme")
     if(theme == "dark"){
@@ -135,14 +140,14 @@
         <svg viewBox="0 0 24 24" width="1em" height="1em" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M3.67981 11.3333H2.92981H3.67981ZM3.67981 13L3.15157 13.5324C3.44398 13.8225 3.91565 13.8225 4.20805 13.5324L3.67981 13ZM5.88787 11.8657C6.18191 11.574 6.18377 11.0991 5.89203 10.8051C5.60029 10.511 5.12542 10.5092 4.83138 10.8009L5.88787 11.8657ZM2.52824 10.8009C2.2342 10.5092 1.75933 10.511 1.46759 10.8051C1.17585 11.0991 1.17772 11.574 1.47176 11.8657L2.52824 10.8009ZM18.6156 7.39279C18.8325 7.74565 19.2944 7.85585 19.6473 7.63892C20.0001 7.42199 20.1103 6.96007 19.8934 6.60721L18.6156 7.39279ZM16.8931 3.60787C16.5403 3.39077 16.0784 3.50074 15.8613 3.8535C15.6442 4.20626 15.7541 4.66822 16.1069 4.88532L16.8931 3.60787ZM12.4633 3.75939C12.877 3.77966 13.2288 3.46071 13.2491 3.047C13.2694 2.63328 12.9504 2.28146 12.5367 2.26119L12.4633 3.75939ZM12.0789 2.25C7.03155 2.25 2.92981 6.3112 2.92981 11.3333H4.42981C4.42981 7.15072 7.84884 3.75 12.0789 3.75V2.25ZM2.92981 11.3333L2.92981 13H4.42981L4.42981 11.3333H2.92981ZM4.20805 13.5324L5.88787 11.8657L4.83138 10.8009L3.15157 12.4676L4.20805 13.5324ZM4.20805 12.4676L2.52824 10.8009L1.47176 11.8657L3.15157 13.5324L4.20805 12.4676ZM19.8934 6.60721C19.1441 5.38846 18.1143 4.35941 16.8931 3.60787L16.1069 4.88532C17.1287 5.51419 17.9899 6.37506 18.6156 7.39279L19.8934 6.60721ZM12.5367 2.26119C12.385 2.25376 12.2323 2.25 12.0789 2.25V3.75C12.2078 3.75 12.336 3.75316 12.4633 3.75939L12.5367 2.26119Z" fill="currentColor"></path> <path d="M11.8825 21V21.75V21ZM20.3137 12.6667H21.0637H20.3137ZM20.3137 11L20.8409 10.4666C20.5487 10.1778 20.0786 10.1778 19.7864 10.4666L20.3137 11ZM18.1002 12.1333C17.8056 12.4244 17.8028 12.8993 18.094 13.1939C18.3852 13.4885 18.86 13.4913 19.1546 13.2001L18.1002 12.1333ZM21.4727 13.2001C21.7673 13.4913 22.2421 13.4885 22.5333 13.1939C22.8245 12.8993 22.8217 12.4244 22.5271 12.1332L21.4727 13.2001ZM5.31769 16.6061C5.10016 16.2536 4.63806 16.1442 4.28557 16.3618C3.93307 16.5793 3.82366 17.0414 4.0412 17.3939L5.31769 16.6061ZM11.5331 20.2423C11.1193 20.224 10.769 20.5447 10.7507 20.9585C10.7325 21.3723 11.0531 21.7226 11.4669 21.7408L11.5331 20.2423ZM7.11292 20.4296C7.4677 20.6433 7.92861 20.529 8.14239 20.1742C8.35617 19.8195 8.24186 19.3586 7.88708 19.1448L7.11292 20.4296ZM11.8825 21.75C16.9448 21.75 21.0637 17.6915 21.0637 12.6667H19.5637C19.5637 16.8466 16.133 20.25 11.8825 20.25V21.75ZM21.0637 12.6667V11H19.5637V12.6667H21.0637ZM19.7864 10.4666L18.1002 12.1333L19.1546 13.2001L20.8409 11.5334L19.7864 10.4666ZM19.7864 11.5334L21.4727 13.2001L22.5271 12.1332L20.8409 10.4666L19.7864 11.5334ZM11.4669 21.7408C11.6047 21.7469 11.7433 21.75 11.8825 21.75V20.25C11.7653 20.25 11.6488 20.2474 11.5331 20.2423L11.4669 21.7408ZM4.0412 17.3939C4.80569 18.6327 5.86106 19.6752 7.11292 20.4296L7.88708 19.1448C6.83872 18.5131 5.95602 17.6405 5.31769 16.6061L4.0412 17.3939Z" fill="currentColor"></path> </g></svg>
       </button>
       <dialog transition:fade={{}} class="rounded-lg h-1/2 aspect-square bg-white dark:bg-gray-800" id="settings" bind:this={settingsDialog} on:click={closeSettings}>
-        <div class="flex flex-col">
+        <div class="flex flex-col h-5/6">
         <button class="self-end" on:click={settingsDialog.close()}><svg viewBox="0 0 24 24" width="1em" height="1em" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M20.7457 3.32851C20.3552 2.93798 19.722 2.93798 19.3315 3.32851L12.0371 10.6229L4.74275 3.32851C4.35223 2.93798 3.71906 2.93798 3.32854 3.32851C2.93801 3.71903 2.93801 4.3522 3.32854 4.74272L10.6229 12.0371L3.32856 19.3314C2.93803 19.722 2.93803 20.3551 3.32856 20.7457C3.71908 21.1362 4.35225 21.1362 4.74277 20.7457L12.0371 13.4513L19.3315 20.7457C19.722 21.1362 20.3552 21.1362 20.7457 20.7457C21.1362 20.3551 21.1362 19.722 20.7457 19.3315L13.4513 12.0371L20.7457 4.74272C21.1362 4.3522 21.1362 3.71903 20.7457 3.32851Z" fill="currentColor"></path> </g></svg></button>
-        <div class="mb-4 border-white rounded-lg border-white">
-          <label class="text-white"><strong>Search Directories Recursively</strong></label>
-          <input type="checkbox" class="form-checkbox mx-2 h-4 w-4" bind:checked={options.recursive}> 
+        <div class="mb-4 border-white rounded-lg border-2 h-full p-2">
+          <label for="recursiveField" class="text-white"><strong>Search Directories Recursively</strong></label>
+          <input id="recursiveField" type="checkbox" class="form-checkbox mx-2 h-4 w-4" bind:checked={options.recursive}> 
         </div>
         </div>
-        <button class="rounded-md self-start" on:click={localStorage.setItem("options", JSON.stringify(options))}>Save</button>
+        <button class="rounded-md" on:click={localStorage.setItem("options", JSON.stringify(options))}>Save</button>
       </dialog>
       <input class="ml-auto rounded-xl px-3 pt-2 right-0" type="text" placeholder="Search" bind:value={searchPattern} on:keypress={search}>
     </div>
@@ -198,7 +203,7 @@
       <thead class="sticky top-0 bg-gray-600">
         <tr>
           <th class="px-2 rounded-l-xl">Cover</th>
-          <th>File Name</th>
+          <th>// File Name</th>
           <th>Song</th>
           <th>Artist</th>
           <th>Year</th>
